4369aba3bc55a0e50ef28f8a93ef3ab1ea9b22a3
YuvCarl <carldrake969@gmail.com>
2025-04-23 22:56:58 +0100

    created the server app and added the user routes aand controlers

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+dotenv.config();
+const db = require('./config/db');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
+// app.use('/api/projects', require('./routes/projectRoutes'));
+// app.use('/api/tasks', require('./routes/taskRoutes'));
+// app.use('/api/inventory', require('./routes/inventoryRoutes'));
+// app.use('/api/finance', require('./routes/financeRoutes'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
