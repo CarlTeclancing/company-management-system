c6b3a47c47f0a114c7addf809341568b612bece2
yuvcarl <carldrake969@gmail.com>
2025-06-18 14:34:57 +0100

    updated the server.js and pus db file

--- server.js
+++ server.js
@@ -7,8 +7,19 @@ dotenv.config();
 const db = require('./config/db');
 
 
+
 const app = express();
-app.use(cors());
+//app.use(cors());
+
+// Allow requests from localhost:5173 (Vite/React frontend)
+app.use(
+    cors({
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+
+    })
+)
+
 app.use(express.json());
 app.use(morgan('dev'));
 
@@ -23,7 +34,7 @@ app.use('/v1/api/company', require('./routes/companyRoutes'));
 app.use('/v1/api/projects', require('./routes/projectRoutes'));
 app.use('/v1/api/tasks', require('./routes/taskRoutes'));
 // app.use('/api/inventory', require('./routes/inventoryRoutes'));
-app.use('v1/api/finance', require('./routes/financeRoutes'));
+app.use('/v1/api/finance', require('./routes/financeRoutes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
