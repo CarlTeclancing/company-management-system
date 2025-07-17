c74e64f5020c496fc97260217447d4b61f92fce8
yuvcarl <carldrake969@gmail.com>
2025-07-17 11:01:26 +0100

    updated the edpoints functions making them async functions

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,38 +1,60 @@
 const db = require('../config/db');
 
-exports.getAllTask = (req, res) => {
+exports.getAllTask = async (req, res) => {
   db.query('SELECT * FROM tasks', (err, results) => {
     if (err) return res.status(500).json({ error: err.message });
     res.status(200).json(results);
   });
 };
 
-exports.getTaskById = (req, res) => {
+exports.getTaskById = async (req, res) => {
   const { id } = req.params;
-  db.query('SELECT * FROM tasks WHERE id = ?', [id], (err, result) => {
-    if (err) return res.status(500).json({ error: err.message });
-    res.status(200).json(result[0]);
-  });
+  try {
+
+    const [result] = await db.query('SELECT * FROM tasks WHERE company_id = ?', [id]);
+    if(result.length === 0){
+      return res.status(404).json({ error: 'No Task found'})
+    }
+  } catch (error) {
+    return res.status(500).json({ error: 'Failed to fetch tasks'});
+  }
 };
 
-exports.createTask = (req, res) => {
-  const { title, description, asignedTo, createdBy, status, priority, dueDate } = req.body;
-  db.query('INSERT INTO tasks (title, description, start_date, end_date, status, created_by) VALUES (?, ?, ?, ?, ?, ?)', [title, description, asignedTo, createdBy, status, priority, dueDate], (err, result) => {
-    if (err) return res.status(500).json({ error: err.message });
-    res.status(201).json({ id: result.insertId, title, description, asignedTo, createdBy, status, priority, dueDate});
+exports.createTask = async (req, res) => {
+try {
+    const { 
+      title,
+      description,
+      priority,
+      assignee,
+      status,
+      date,
+      user_id,
+      company_id,
+   } = req.body;
+  db.query('INSERT INTO tasks (	title,	description,	assigned_to,	created_by,	status,	priority,	due_date, company_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [title, description, assignee, user_id, status, priority, date, company_id], (err, result) => {
+    res.status(201).json({
+      success: true,
+      message: "Project created successfully.",
+      data: { id: result.insertId, title, description, priority, assignee, status, date, user_id, company_id }
+    });
   });
-};
+} catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+  
+}
 
-exports.updateTask = (req, res) => {
+exports.updateTask = async (req, res) => {
   const { id } = req.params;
-  const { title, description, asignedTo, createdBy, status, priority, dueDate } = req.body;
-  db.query('UPDATE tasks SET name = ?, description = ?, start_date = ?, end_date = ?, status = ?, created_by = ? WHERE id = ?', [title, description, asignedTo, createdBy, status, priority, dueDate], (err) => {
+  const { title, description, priority, assignee, status, date, user_id, company_id } = req.body;
+  db.query('UPDATE tasks SET title = ?, description = ?, priority = ?, assigned_to = ?, status = ?, due_date = ?, company_id = ? WHERE id = ?', [title, description, priority, assignee, status, date, company_id, id], (err) => {
     if (err) return res.status(500).json({ error: err.message });
-    res.status(200).json({ id, title, description, asignedTo, createdBy, status, priority, dueDate });
+    res.status(200).json({ id, title, description, priority, assignee, status, date, user_id, company_id });
   });
 };
 
-exports.deleteTask = (req, res) => {
+exports.deleteTask = async (req, res) => {
   const { id } = req.params;
   db.query('DELETE FROM tasks WHERE id = ?', [id], (err) => {
     if (err) return res.status(500).json({ error: err.message });
