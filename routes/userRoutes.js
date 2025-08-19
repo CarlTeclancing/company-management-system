aec9eadb1835cecb77f015af73c708a078c36599
yuvcarl <carldrake969@gmail.com>
2025-08-19 20:48:25 +0100

    udpated the getusers that belong to a single company

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUsers, getUserById, getUserByCompanyId, createUser, updateUser, deleteUser } = require('../controllers/userController');
+const { getAllUsers, getUserById, getUsersByCompanyId, createUser, updateUser, deleteUser } = require('../controllers/userController');
 
 router.get('/', getAllUsers);
 router.get('/:id', getUserById);
-router.get('/company/:id', getUserByCompanyId);
+router.get('/company/:id', getUsersByCompanyId);
 router.post('/', createUser);
 router.put('/:id', updateUser);
 router.delete('/:id', deleteUser);
